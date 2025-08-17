77e75068fbfbb2214e2677e3f0ccdbcc8d71a535
harsha86396 <harsha86396@gmail.com>
2025-08-17 16:35:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from app import create_app
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Sudoku App is running on Render!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
