bfac4ba694664c2ff86c111d933d54a482fa114b
harsha86396 <harsha86396@gmail.com>
2025-08-17 19:11:59 +0530

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,16 +1,37 @@
+const CACHE_NAME = "sudoku-cache-v2";
+const URLS_TO_CACHE = [
+  "/",
+  "/static/style.css",
+  "/static/main.js",
+  "/static/icons/icon-192.png",
+  "/static/icons/icon-512.png"
+];
+
+// Install and cache assets
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("sudoku-cache-v1").then(cache => {
-      return cache.addAll([
-        "/",
-        "/static/style.css",
-        "/static/icons/icon-192.png",
-        "/static/icons/icon-512.png"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
 });
 
+// Activate and clear old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch from cache or network
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
